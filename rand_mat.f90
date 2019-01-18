d534dc7fa6f71fe6327cf800c43a8694b5634101
Christian <christianrbolander@gmail.com>
2019-01-18 11:35:32 -0700

    Documentation for rand_mat and shared library added.

--- rand_mat.f90
+++ rand_mat.f90
@@ -2,5 +2,7 @@ SUBROUTINE rand_mat(r, c, mat)
 	IMPLICIT NONE
 	INTEGER, INTENT(in) :: r, c
 	REAL*8, INTENT(out) ::  mat(r, c)
+	! Fill a matrix with `r` rows and `c` columns with a random
+	! number between 0 and 1 (not including 1).
 	CALL RANDOM_NUMBER(mat)
 END SUBROUTINE
