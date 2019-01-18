7b1104dbb87fa548e162cf5235080b3d4f9746e6
Christian <christianrbolander@gmail.com>
2019-01-18 11:33:26 -0700

    Rewritten smaceps and dmaceps in Fortran.

---
+++ dmaceps.f90
@@ -0,0 +1,30 @@
+SUBROUTINE dmaceps(mach_eps, i)
+	IMPLICIT NONE
+	
+	REAL*8, INTENT(OUT) :: mach_eps
+	INTEGER, INTENT(OUT) :: i
+	REAL*8 :: one, approx
+	
+	! Initialize variables. These are set an arbitrary number that we are
+	! going to use to determine the machine precision of the computer.
+	one = 1.0
+	mach_eps = 1.0
+	approx = one + mach_eps
+	
+	! Do loop to calculate machine precision. Divides the arbitrary
+	! number chosen above by 2 at each iteration until the computer
+	! cannot tell the difference between `approx` and `one`.
+	DO i=1, 1000
+		mach_eps = mach_eps / 2.
+		approx = one + mach_eps
+		
+		! If the computer cannot tell the difference between `approx`
+		! and `one`, the subroutine exits with `i` (the number of
+		! iterations), and `mach_eps` (machine precision).
+		IF(ABS(approx - one) == 1.0) RETURN
+	
+	END DO
+	
+	WRITE(*,*) "Loop limit exceeded."
+	
+END SUBROUTINE
--- rand_mat.f90
+++ rand_mat.f90
@@ -1,6 +1,6 @@
 SUBROUTINE rand_mat(r, c, mat)
-		IMPLICIT NONE
-		INTEGER, INTENT(in) :: r, c
-		REAL*8, INTENT(out) ::  mat(r, c)
-		CALL RANDOM_NUMBER(mat)
+	IMPLICIT NONE
+	INTEGER, INTENT(in) :: r, c
+	REAL*8, INTENT(out) ::  mat(r, c)
+	CALL RANDOM_NUMBER(mat)
 END SUBROUTINE
